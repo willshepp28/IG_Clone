085cd3becf8cd39dd040eda0df7a9f63fc03fa8d
willshepp28 <willsheppard28@outlook.com>
2017-12-01 13:45:20 -0500

    switched to bootstrap 4 , included user info on posts through inner join

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,9 @@ router
 
         var posts = await knex.select()
             .from('posts')
+            .innerJoin('users', 'user_id', 'users.id')
             .then((post) => {
+            
                 response.render('home', { post });
             })
             .catch((err) => {
@@ -42,6 +44,24 @@ router
 
     })
     .post((request, response) => {
+
+    });
+
+
+
+// profile:id
+router 
+    .route('/profile:id')
+    .get(async(request, response) => {
+
+    });
+
+
+
+// tags:hastag
+router 
+    .route('/tags:hastag')
+    .get(async(request, response) => {
 
     });
 
