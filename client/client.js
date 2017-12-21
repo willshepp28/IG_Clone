05f84ad33e4cab11938780654c3834dfd82d3683
willshepp28 <willsheppard28@outlook.com>
2017-12-20 19:21:02 -0500

    finished refactor for now

---
+++ client/client.js
@@ -0,0 +1,23 @@
+// Client.JS
+
+
+const API_URL = 'http://localhost:8000/api/v1/user';
+
+
+
+
+
+
+$(() => {
+    $.get(API_URL)
+        .then(user => {
+
+            // Get the class with the id of users
+            const $user = $('#users');
+
+
+            user.forEach(i => {
+                $user.append(`<h2>${i.password}</h2><br>`);
+            })
+        })
+});
